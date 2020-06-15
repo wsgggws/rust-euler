17479143d6752bb54f3d59442e067d148ca5d506
wsgggws <wsgggws@gmail.com>
2020-06-15 20:38:08 +0800

    Add src/p0005_smallest_multiple.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod p0001_multiples_of_3_and_5;
 pub mod p0002_even_fibonacci_numbers;
 pub mod p0003_largest_prime_factor;
 pub mod p0004_largest_palindrome_product;
+pub mod p0005_smallest_multiple;
 pub mod p0006_sum_square_difference;
 pub mod p0007_10001st_prime;
 pub mod p0008_largest_product_in_a_series;
