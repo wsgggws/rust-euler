7906ba4db9cbcb6cadae3aacf9eaf97efbfb14bf
wsgggws <wsgggws@gmail.com>
2020-06-15 19:13:32 +0800

    Add p0013_large_sum p0018_maximum_path_sum_i p0021_amicable_numbers p0023_non_abundant_sums p0067_maximum_path_sum_ii

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ pub mod p0009_special_pythagorean_triplet;
 pub mod p0010_summation_of_primes;
 pub mod p0011_largest_product_in_a_grid;
 // pub mod p0012_highly_divisible_triangular_number;
+pub mod p0013_large_sum;
 pub mod p0015_lattice_paths;
 pub mod p0016_power_digit_sum;
+pub mod p0018_maximum_path_sum_i;
 pub mod p0020_factorial_digit_sum;
+pub mod p0021_amicable_numbers;
+pub mod p0023_non_abundant_sums;
+pub mod p0067_maximum_path_sum_ii;
 
 fn main() {
     println!("Hello, rust and euler");
