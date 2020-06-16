1b1c76b7324684cdd5085221b3779ab251d91a3a
wsgggws <wsgggws@gmail.com>
2020-06-16 20:33:22 +0800

    Add src/p0024_lexicographic_permutations.rs src/p0025_1000_digit_fibonacci_number.rs src/p0028_number_spiral_diagonals.rs src/p0029_distinct_powers.rs src/p0030_digit_fifth_powers.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ pub mod p0008_largest_product_in_a_series;
 pub mod p0009_special_pythagorean_triplet;
 pub mod p0010_summation_of_primes;
 pub mod p0011_largest_product_in_a_grid;
-// pub mod p0012_highly_divisible_triangular_number;
 pub mod p0013_large_sum;
 pub mod p0015_lattice_paths;
 pub mod p0016_power_digit_sum;
@@ -17,6 +16,11 @@ pub mod p0018_maximum_path_sum_i;
 pub mod p0020_factorial_digit_sum;
 pub mod p0021_amicable_numbers;
 pub mod p0023_non_abundant_sums;
+pub mod p0024_lexicographic_permutations;
+pub mod p0025_1000_digit_fibonacci_number;
+pub mod p0028_number_spiral_diagonals;
+pub mod p0029_distinct_powers;
+pub mod p0030_digit_fifth_powers;
 pub mod p0067_maximum_path_sum_ii;
 
 fn main() {
