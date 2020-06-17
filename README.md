8bbbb224ed51ebb2a20ff62431f45d72bbce7ae0
hjtianvip <hjtianvip@gmail.com>
2020-06-17 12:28:05 +0800

    Update README

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Project euler solved by Rust
 
 ## Project euler
 - [https://projecteuler.net](https://projecteuler.net/archives)
+- rustc: 1.44
+
+## Test
+- cargo test
+
+My Own Key:
+`1574526_vCbfhdfnB0B45vkObMkAdnU1MVDEU2fO`
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,63 @@
+// 基础题
 pub mod p0001_multiples_of_3_and_5;
 pub mod p0002_even_fibonacci_numbers;
 pub mod p0003_largest_prime_factor;
 pub mod p0004_largest_palindrome_product;
 pub mod p0005_smallest_multiple;
 pub mod p0006_sum_square_difference;
-pub mod p0007_10001st_prime;
 pub mod p0008_largest_product_in_a_series;
-pub mod p0009_special_pythagorean_triplet;
-pub mod p0010_summation_of_primes;
-pub mod p0011_largest_product_in_a_grid;
-pub mod p0012_highly_divisible_triangular_number;
-pub mod p0013_large_sum;
+// 17, 22
+
+// 序列
 pub mod p0014_longest_collatz_sequence;
-pub mod p0015_lattice_paths;
-pub mod p0016_power_digit_sum;
-pub mod p0018_maximum_path_sum_i;
-pub mod p0020_factorial_digit_sum;
+// 92
+
+// 因子
+pub mod p0012_highly_divisible_triangular_number;
 pub mod p0021_amicable_numbers;
 pub mod p0023_non_abundant_sums;
-pub mod p0024_lexicographic_permutations;
-pub mod p0025_1000_digit_fibonacci_number;
+// 47
+
+// 素数
+pub mod p0007_10001st_prime;
+pub mod p0010_summation_of_primes;
+// 27, 35, 37, 50, 58, 97
+
+// 数字游戏
+pub mod p0009_special_pythagorean_triplet;
+pub mod p0011_largest_product_in_a_grid;
 pub mod p0028_number_spiral_diagonals;
-pub mod p0029_distinct_powers;
 pub mod p0030_digit_fifth_powers;
+// 32, 34, 36, 38, 40, 46, 52, 206
+
+// 大整数
+pub mod p0013_large_sum;
+pub mod p0016_power_digit_sum;
+pub mod p0020_factorial_digit_sum;
+pub mod p0025_1000_digit_fibonacci_number;
+pub mod p0029_distinct_powers;
+// 48, 53, 55, 56, 57, 63
+
+// 路径
+pub mod p0015_lattice_paths;
+pub mod p0018_maximum_path_sum_i;
 pub mod p0067_maximum_path_sum_ii;
+
+// 排列组合
+pub mod p0024_lexicographic_permutations;
+// 31, 41, 49, 43
+
+// 日期
+// 19
+
+// 分数
+// 26, 33
+
+// 三角形数
+// 39, 42, 44, 45
+
+// 密码学
+// 59, 79
 
 fn main() {
     println!("Hello, rust and euler");
